6e34ef650b910507c87fbc9102f386188a976c97
Caglar <Caglar@DESKTOP-4EQGD41>
2024-05-14 15:24:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var singleService = new SingleIntegrationService();
+
+var tasks = new[]
+{
+            singleService.ProcessItemAsync("content1"),
+            singleService.ProcessItemAsync("content2"),
+            singleService.ProcessItemAsync("content1"), // Yinelenen içerik
+            singleService.ProcessItemAsync("content3"),
+            singleService.ProcessItemAsync("content2")  // Yinelenen içerik
+        };
+
+var results = await Task.WhenAll(tasks);
+
+foreach (var result in results)
+{
+    Console.WriteLine(result);
+}
+
+
+
+var distributedService = new DistributedItemIntegrationService();
+
+var tasks1= new[]
+{
+            distributedService.ProcessItemAsync("content1"),
+            distributedService.ProcessItemAsync("content2"),
+            distributedService.ProcessItemAsync("content1"), // Yinelenen içerik
+            distributedService.ProcessItemAsync("content3"),
+            distributedService.ProcessItemAsync("content2")  // Yinelenen içerik
+        };
+
+var results1 = await Task.WhenAll(tasks1);
+
+foreach (var result in results1)
+{
+    Console.WriteLine(result);
+}
+
+
+
+
+
